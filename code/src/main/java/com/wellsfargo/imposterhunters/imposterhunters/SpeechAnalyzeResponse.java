7e0a34e3c89101dd18296bd778ffefdb95fe0e05
shivaprakash22 <105652182+shivaprakash22@users.noreply.github.com>
2024-03-17 19:00:50 +0530

    Update SpeechAnalyzeResponse.java

--- code/src/main/java/com/wellsfargo/imposterhunters/imposterhunters/SpeechAnalyzeResponse.java
+++ code/src/main/java/com/wellsfargo/imposterhunters/imposterhunters/SpeechAnalyzeResponse.java
@@ -1,4 +1,14 @@
 package com.wellsfargo.imposterhunters.imposterhunters;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class SpeechAnalyzeResponse {
+    private String text;
+    private String status;
+    private double positivescore;
+    private double neutralscore;
+    private double negativescore;
 }
